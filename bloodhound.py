c353913429dbc135dce58d0c8ea63c35a4bfc4d9
Brukusec <bruno.monteiro1980@gmail.com>
2024-06-18 11:32:43 +0100

    Update bloodhound.py

--- bloodhound.py
+++ bloodhound.py
@@ -1,5 +1,12 @@
 #! /usr/bin/env python
 
 import bloodhound
+from bloodhound.ldap_pool import LDAPConnectionPool
 
-bloodhound.main()
+def main():
+    # Inicialize o pool de conexões LDAP aqui, se necessário
+    ldap_pool = LDAPConnectionPool(server_address, username, password)
+    bloodhound.main()
+
+if __name__ == "__main__":
+    main()
